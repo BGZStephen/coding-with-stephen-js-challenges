cb7a09ab3de93f550f98a3783f5cecbee1d44514
Stephen <sjw948@gmail.com>
2021-08-02 18:25:14 +0100

    fix assertion

--- challenges/basics/get-middle-item-from-array/get-middle-item-from-array.test.js
+++ challenges/basics/get-middle-item-from-array/get-middle-item-from-array.test.js
@@ -4,18 +4,18 @@ describe("getMiddleItem", () => {
   test('returns [] for []', () => {
     const res = getMiddleItem([])
 
-    expect(res).toBe([])
+    expect(res).toEqual([])
   })
 
   test('returns 2 for [1, 2, 3]', () => {
     const res = getMiddleItem([1, 2, 3])
 
-    expect(res).toBe([2])
+    expect(res).toEqual([2])
   })
 
   test('returns [2, 3] for [1, 2, 3, 4]', () => {
     const res = getMiddleItem([1, 2, 3, 4])
 
-    expect(res).toBe([2, 3])
+    expect(res).toEqual([2, 3])
   })
 })
