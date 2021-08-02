fb048aaee954da7b86256a1d68c6dd53b7a52e79
Stephen <sjw948@gmail.com>
2021-08-02 18:53:51 +0100

    remove solution

--- challenges/beginner/fizz-buzz/fizz-buzz.js
+++ challenges/beginner/fizz-buzz/fizz-buzz.js
@@ -1,18 +1,5 @@
 function fizzBuzz(number) {
-  let res = "";
-  for (let i = 1; i <= number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      res += "fizzBuzz\n"
-    } else if (i % 5 === 0) {
-      res += "buzz\n"
-    } else if (i % 3 === 0) {
-      res += "fizz\n"
-    } else {
-      res += `${i}\n`
-    }
-  }
 
-  return res;
 }
 
 module.exports = {
